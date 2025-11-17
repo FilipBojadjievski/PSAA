a7127a4c7223a6c02ee1009f8c01e954e00f8aaa
Filip <Filip.bojadzievski@gmail.com>
2025-11-17 12:41:51 +0100

    Zad2

---
+++ Kolok2023/Zad2.java
@@ -0,0 +1,139 @@
+import java.lang.Math;
+import java.util.Scanner;
+class Node<E> {
+    protected E data;
+    protected Node<E> next;
+    
+    public Node() {
+        data = null;
+        next = null;
+    }
+    
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class SLinkedList<E> {
+    private Node<E> head;
+    
+    public Node<E> getHead() {
+        return head;
+    }
+    
+    public void setHead(Node<E> n) {
+        head = n;
+    }
+    
+    public SLinkedList() {
+        head = null;
+    }
+    
+    public void insertFirst(E e) {
+        Node<E> first = new Node(e, head);
+        head = first;
+    }
+    
+    public void insertAfter(E e, Node<E> n) {
+        if (n != null) {
+            Node<E> node = new Node(e, n.next);
+            n.next = node;
+        } else {
+            System.out.println("Error.");
+        }
+    }
+    
+    public void insertBefore(E e, Node<E> n) {
+        if (head != null) {
+            Node<E> tmp = head;
+            if (tmp == n) {
+                this.insertFirst(e);
+                return;
+            }
+            
+            while(tmp.next != n && tmp.next != null) {
+                tmp = tmp.next;
+            }
+            
+            if (tmp.next == n) {
+                Node<E> node = new Node(e, n);
+                tmp.next = node;
+            }
+        }
+    }
+    
+    public void insertLast(E e) {
+        if (head != null) {
+            Node<E> tmp = head;
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+            
+            Node<E> last = new Node(e, null);
+            tmp.next = last;
+        } else {
+            this.insertFirst(e);
+        }
+    }
+    
+    public void deleteFirst() {
+        if (head != null) {
+            head = head.next;
+        } else {
+            System.out.println("Error.");
+        }
+    }
+    
+    public int size() {
+        Node<E> tmp = head;
+        int size = 0;
+        
+        while (tmp != null) {
+            size++;
+            tmp = tmp.next;
+        }
+        
+        return size;
+    }
+    
+    public void printList() {
+        Node<E> tmp = head;
+        
+        while (tmp.next != null) {
+            System.out.print(tmp.data + " -> ");
+            tmp = tmp.next;
+        }
+        System.out.println(tmp.data);
+    }
+}
+
+public class Zad2 {
+    static SLinkedList <Integer> lista = new SLinkedList <Integer>();
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        Integer n=in.nextInt();
+        for (int i=0;i<n;i++){
+            lista.insertLast(in.nextInt());
+        }
+        System.out.println(value());
+    }
+    public static Integer value (){
+        if (lista.getHead()==null){
+            return 0;
+        }
+        if (lista.getHead().next==null){
+            return lista.getHead().data;
+        }
+        Integer prv=lista.getHead().data;
+        Integer vtor=lista.getHead().next.data;
+        Node <Integer> cur=lista.getHead().next.next;
+        while (cur!=null) {
+            Integer temp=Math.min(prv,vtor)+cur.data;
+            cur=cur.next;
+            prv=vtor;
+            vtor=temp;
+        }
+        return Math.min(prv,vtor);
+    }
+}
